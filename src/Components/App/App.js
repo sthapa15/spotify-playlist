9e11272571ed51730342c8e9e7a2ee5504b32cdd
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-01-23 15:00:21 -0500

    add mock data to set up passin gprops and state between components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,67 @@
 import "./App.css";
 import React from "react";
-// import { SearchBar } from "./SearchBar";
+import SearchBar from "../SearchBar/SearchBar";
+import Playlist from "../Playlist/Playlist";
+import SearchResults from "../SearchResults/SearchResults";
 // import { Playlist } from "./Playlist";
 // import { SearchResults } from "./SearchResults";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [
+        {
+          name: "Mr. Brightside",
+          artist: "The Killers",
+          album: "Dunno",
+          id: 1,
+        },
+        { name: "Hey Jude", artist: "The Beatles", album: "Dunno2", id: 2 },
+        { name: "Sk8er Boi", artist: "Avril Lavigne", album: "Dunno3", id: 3 },
+      ],
+      playlistName: "Fav Songs",
+      playlistTracks: [
+        {
+          name: "Yellow Submarine",
+          artist: "The Beatles",
+          album: "Yellow Submarine",
+          id: "id1",
+        },
+        {
+          name: "Hey Ya",
+          artist: "Outkast",
+          album: "something",
+          id: "id2",
+        },
+        {
+          name: "Rude Boy",
+          artist: "Rihanna",
+          album: "gold",
+          id: "id3",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>
-          Ja<span className="highlight">mmm</span>ing
+          Musical <span className="highlight">Zen</span>
         </h1>
         <div className="App">
-          {/* <SearchBar /> */}
-          <div className="App-playlist"></div>
-          {/* <SearchResults /> */}
-          {/* <Playlist /> */}
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist
+              playlistTracks={this.state.playlistTracks}
+              playlistName={this.state.playlistName}
+            />
+          </div>
         </div>
-      </div> 
+      </div>
     );
   }
 }
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "reactDOM";
+import "./SearchBar.css";
 
 class SearchBar extends React.Component {
   render() {
-    return <div className="SearchBar">
+    return (
+      <div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist" />
         <button className="SearchButton">SEARCH</button>
-    </div>;
+      </div>
+    );
   }
 }
 

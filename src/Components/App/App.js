55b4e60095f38de6456978c39924b588f75b1039
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-01-23 16:46:43 -0500

    add remove track and add track functionality with mock data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -43,6 +43,26 @@ class App extends React.Component {
         },
       ],
     };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -54,10 +74,14 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
             <Playlist
               playlistTracks={this.state.playlistTracks}
               playlistName={this.state.playlistName}
+              onRemove={this.removeTrack}
             />
           </div>
         </div>
